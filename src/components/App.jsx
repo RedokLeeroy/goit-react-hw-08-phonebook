d1d33700b611c213777307075e65660946cbaf20
RedokLeeroy <Lys1999@ukr.net>
2022-08-12 16:55:02 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { Phonebook } from '../pages/Phonebook/Phonebook';
 import { RegisterForm } from '../pages/register/Register';
-import { Login } from '../pages/Login/Login';
+import { Login } from 'pages/Login/Login';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getRefresh } from 'redux/auth/auth-operations';
